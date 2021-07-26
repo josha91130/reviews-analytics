e31a4dcbccdaf0b1a5369baad843e37f0a02939b
Josh Lin <josha91130@gmail.com>
2021-07-26 19:10:25 +0800

    add filter

--- read.py
+++ read.py
@@ -8,4 +8,23 @@ with open('C:/Users/josha/Desktop/reviews-analytics/reviews.txt', 'r') as f:
         data.append(line)
         if count % 1000 == 0:
             print(len(data))
-print(len(data))
+print('檔案讀取完畢, 總共有', len(data), '筆資料')
+
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new),'筆資料小於100個字')
+
+good = []
+for g in data:
+    if 'good' in g:
+        good.append(g)
+print('一共有', len(good),'筆資料有good')
+print(good[0])
+
+
+
+
+
+
